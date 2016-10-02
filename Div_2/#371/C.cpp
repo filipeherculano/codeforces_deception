4ea80fe6681fb4955c8ac35361291857af0120e2
Filipe <filipeherculanorocha@gmail.com>
2016-10-02 13:24:53 -0300

    finally

--- Div_2/#371/C.cpp
+++ Div_2/#371/C.cpp
@@ -1,68 +1,49 @@
 #include <bits/stdc++.h>
-#define FOR(i, n) for(int i = 0; i < n; i++)
+
+#define FOR(i, a, n) for(int i = (int)(a); i < (int)(n); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define all(a) a.begin(),a.end()
+#define pb push_back
 
 typedef unsigned long long llu;
 typedef long long ll;
-typedef long double Lf;
+typedef long double ld;
 
 using namespace std;
 
-struct node{
-	struct node *father;
-	struct node *zero;
-	struct node *one;
-	int bit; // zero or one
-	int value;
-	int qnt;
-}
-
-struct node _insert(struct node root, int num, int bit, int v){
-	if(bit > 19) return root;
-	if(num&1){
-		if(root.one == NULL){
-			struct node temp;
-			temp.bit = 1;
-			temp.value = v;
-			temp.qnt = 0;
-			temp.father = root;
-			temp.one = NULL;
-			temp.zero = NULL;
-			root.one = temp;
-		} else root = _insert(root.one, num >> 1, bit++, v);
-	} else {
-		if(root.zero == NULL){
-			struct node temp;
-			temp.bit = 1;
-			temp.value = v;
-			temp.qnt = 0;
-			temp.father = root;
-			temp.one = NULL;
-			temp.zero = NULL;
-			root.one = temp;
-		} else root = _insert(root.zero, num >> 1, bit++, v);
+long long hashFunction(long long num){
+	long long res = 0, pot = 1;
+	while(num){
+		int aux = num % 10;
+		if(aux % 2) res += pot;
+		pot *= 2;
+		num /= 10;
 	}
-	if(bit == 19) temp.qnt++;
-	return root;
-}
-
-struct node _delete(struct node root, int num, int bit){
-	if(num&1){
-		if(root.one != NULL){
-			root = _delete(root.one, num >> 1, bit++);
-		} else {
-			if(root.qnt > 1) root.qnt--;
-			else{
-				if(root.father != NULL) root = root.father;
-			}
-		}
-	} else {
-	
-	}
+	return res;
 }
 
 int main(){
-	int t, num;
-	scanf("%d%*c", &t);
+	int t;
+	long long num;
 	char c;
+	scanf("%d%*c", &t);
+	int hash[262144];
+	REP(i, 262144) hash[i] = 0;
+	while(t--){
+		cin >> c >> num;
+		if(c == '+') hash[hashFunction(num)]++;
+		else if(c == '-') hash[hashFunction(num)]--;
+		else if(c == '?') cout << hash[hashFunction(num)] << endl;
+	}
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
